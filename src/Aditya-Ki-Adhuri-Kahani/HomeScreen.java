55cb784cd1d804cd6b3711cbea1f7b578c31e71d
BhavikPawar29 <144596999+BhavikPawar29@users.noreply.github.com>
2023-11-24 14:04:47 +0530

    Update HomeScreen.java

--- src/Aditya-Ki-Adhuri-Kahani/HomeScreen.java
+++ src/Aditya-Ki-Adhuri-Kahani/HomeScreen.java
@@ -1,58 +1,62 @@
-//package quizproject;
-
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class HomeScreen extends JFrame implements ActionListener{
+public class HomeScreen extends JFrame implements ActionListener {
 
-    JButton btnCreate,btnResult,btnStart;
+    JButton btnCreate, btnResult, btnStart;
 
-    public HomeScreen(){
+    public HomeScreen() {
         super("Quiz Application");
         this.setLayout(null);
-        this.setSize(1280,720);
+        this.setSize(800, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
         this.setLocationRelativeTo(null);
 
-        this.btnCreate=new JButton("Create New Quiz");
+        int buttonWidth = 200;
+        int buttonHeight = 60;
+        int horizontalGap = 20;
+        int verticalGap = 40;
+
+        this.btnCreate = new JButton("Create New Quiz");
         this.btnCreate.addActionListener(this);
-        this.btnCreate.setBounds(200,200,200,60);
+        this.btnCreate.setBounds(300, 150, buttonWidth, buttonHeight);
         this.add(btnCreate);
 
-        this.btnStart=new JButton("Start the Quiz");
+        this.btnStart = new JButton("Start the Quiz");
         this.btnStart.addActionListener(this);
-        this.btnStart.setBounds(800,200,200,60);
+        this.btnStart.setBounds(300, 250, buttonWidth, buttonHeight);
         this.add(btnStart);
 
-        this.btnResult=new JButton("View Result");
+        this.btnResult = new JButton("View Result");
         this.btnResult.addActionListener(this);
-        this.btnResult.setBounds(500,400,200,60);
+        this.btnResult.setBounds(300, 350, buttonWidth, buttonHeight);
         this.add(btnResult);
 
+        this.setVisible(true);
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==btnCreate){
+        if (e.getSource() == btnCreate) {
             this.setVisible(false);
-            new CreateQuiz();
+            new CreateQuiz().setVisible(true);
         }
 
-        if(e.getSource()==btnStart){
+        if (e.getSource() == btnStart) {
             this.setVisible(false);
-            StartQuiz sq = new StartQuiz();
+            StartQuiz startQuiz = new StartQuiz();
+            startQuiz.setVisible(true);
         }
-                
-        if(e.getSource()==btnResult){
+
+        if (e.getSource() == btnResult) {
             this.setVisible(false);
-            ViewResult vr = new ViewResult();
+            // ViewResult viewResult = new ViewResult();
+            // viewResult.setVisible(true);
         }
-        
     }
 
     public static void main(String[] args) {
-        new HomeScreen();
+        SwingUtilities.invokeLater(() -> new HomeScreen());
     }
 }
